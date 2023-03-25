1a07f7eb77e66a444d29d873bf16661f9c22cc1a
khake19 <draudekrek@gmail.com>
2023-03-25 11:07:06 +0800

    add filter component

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Select from 'react-select';
+import FilterStyle from './Filter.module.css';
+import useFiltersStore from '../../stores/filters'
+
+const platformOptions = [
+  { value: 'all', label: 'All' },
+  { value: 'pc', label: 'PC' },
+  { value: 'browser', label: 'Browser' }
+];
+
+const sortByOptions = [
+  { value: 'release-date', label: 'Release Date' },
+  { value: 'alphabetical', label: 'Alphabetical' },
+  { value: 'relevance', label: 'Relevance' }
+];
+
+const Filter = () => {
+  const addPlatform = useFiltersStore((state) => state.addPlatform)
+  const addSortBy = useFiltersStore((state) => state.addSortBy)
+
+
+  return <div className={FilterStyle.container}>
+      <div className={FilterStyle.platform}>
+        <label className={FilterStyle.label}>Filter by Platform</label>
+        <Select
+          className={FilterStyle.inputField}
+          defaultValue={platformOptions[0]}
+          onChange={(platform) => addPlatform(platform.value)}
+          options={platformOptions}
+        />
+      </div>
+      <div className={FilterStyle.platform}>
+        <label className={FilterStyle.label}>Sort By</label>
+        <Select
+          className={FilterStyle.inputField}
+          defaultValue={sortByOptions[0]}
+          onChange={(sortBy) => addSortBy(sortBy.value)}
+          options={sortByOptions}
+        />
+      </div>
+  </div>
+}
+
+export default Filter
