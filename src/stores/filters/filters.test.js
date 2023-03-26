cdef3ddaad3a4e4fc6abbef6ae1d551409266ea6
khake19 <draudekrek@gmail.com>
2023-03-26 18:21:59 +0800

    clean up filters test

--- src/stores/filters/filters.test.js
+++ src/stores/filters/filters.test.js
@@ -3,20 +3,32 @@ import { act } from '@testing-library/react-hooks'
 
 import useFiltersStore  from './filters'
 
-test('useFiltersStore', () => {
-  const { result } = renderHook(() => useFiltersStore());
+describe('useFiltersStore' , () => {
+  it('should add a platform', () => {
+    const { result } = renderHook(() => useFiltersStore());
 
-  expect(result.current.categories).toStrictEqual([])
+    act(() => result.current.addPlatform('PC'))
+    expect(result.current.platform).toStrictEqual('PC')
 
-  act(() => result.current.addPlatform('PC'))
-  expect(result.current.platform).toStrictEqual('PC')
+  })
+  it('should add a sort', () => {
+    const { result } = renderHook(() => useFiltersStore());
 
-  act(() => result.current.addSortBy('relevant'))
-  expect(result.current.sortBy).toStrictEqual('relevant')
+    act(() => result.current.addSortBy('relevant'))
+    expect(result.current.sortBy).toStrictEqual('relevant')
+  })
+  it('should add a categories', () => {
+    const { result } = renderHook(() => useFiltersStore());
 
-  act(() => result.current.addCategory(['id1']))
-  expect(result.current.categories).toStrictEqual(['id1'])
+    expect(result.current.categories).toStrictEqual([])
 
-  act(() => result.current.addSearch('lorem'))
-  expect(result.current.search).toStrictEqual('lorem')
+    act(() => result.current.addCategory(['id1']))
+    expect(result.current.categories).toStrictEqual(['id1'])
+  })
+  it('should add a search', () => {
+    const { result } = renderHook(() => useFiltersStore());
+
+    act(() => result.current.addSearch('lorem'))
+    expect(result.current.search).toStrictEqual('lorem')
+  })
 })
